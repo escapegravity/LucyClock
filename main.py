7d46634e8bc95a4340d6d0374746b904fddf374a
Joakim Olsson <joakim.olsson90@gmail.com>
2020-12-22 15:08:44 +0100

    change time and photos

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def index():
     if now.today().weekday() > 4:
         morningShift = now.replace(hour=7, minute=30, second=00)
     else:
-        morningShift = now.replace(hour=7, minute=00, second=00)
+        morningShift = now.replace(hour=7, minute=30, second=00)
 
-    nightShift = now.replace(hour=20, minute=00, second=00)
+    nightShift = now.replace(hour=20, minute=30, second=00)
      
     if now < morningShift or now > nightShift:
         template = 'night.html'
